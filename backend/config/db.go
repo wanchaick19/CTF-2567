d4bc608a3b8493ccba67cdfbfb1c0c9be527c5eb
wanchaick19 <fourarm0500@gmail.com>
2025-01-12 13:50:59 +0700

    ajanparin problem

--- backend/config/db.go
+++ backend/config/db.go
@@ -32,12 +32,21 @@ func SetupDatabase() {
 
 		// Insert default tags
 		flags := []entity.Flags{
-			{Flag: "testflag"},
-
+			{Flag: "testflag"},{Flag: "testflag2"},{Flag: "cc26fe5e527612aef91542b11eaf501440ff6a03ef8ec20a358bfdf85d9f7813"},
 		}
 	
 		for _, flag := range flags {
 			db.FirstOrCreate(&flag, entity.Flags{Flag: flag.Flag})
+		}
+
+		// Insert default tags
+		keys := []entity.Keys{
+			{Key: "testflag"},{Key: "testflag2"},{Key: "key-ajanparin"},
+
+		}
+	
+		for _, key := range keys {
+			db.FirstOrCreate(&key, entity.Keys{Key: key.Key})
 		}
 	
 }
