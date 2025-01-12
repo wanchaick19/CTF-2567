59acfdd25dc2d4728f89a2b00b6cc6b50c3504ef
Lain <46403242+CharliezXx@users.noreply.github.com>
2025-01-12 16:41:00 +0700

    lev1

---
+++ frontend/src/components/Level1.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Lee.css"
+const Level1: React.FC = () => {
+    return (
+        
+        <>
+            <div className='box'>
+                <h1 className='str'>Level 01</h1><br />
+            </div>
+            <div className='box'>
+                <p className='hidden_wall'>NVXOUExP2TTnlwuMySAFBQ==</p>
+            </div>
+            <div className='box'>
+                <p>ชื่อ <strong className='str'>16 อักษร</strong>เป็นกุญแจสู่คำตอบ<br />4 คือเวลาทั้งหมด (ลงท้ายด้วย 7)</p>
+            </div>
+            <div className='btn_bg'>
+                <a href="https://www.youtube.com/watch?v=poa_QBvtIBA" target='_blank'><button className='btn'>key</button></a>
+            </div>
+            <div className='btn_bg'><a href="https://anycript.com/crypto" target='_black'><button className='btn'>Decrypt</button></a></div>
+        </>
+    );
+};
+
+export default Level1
+
--- frontend/src/pages/Game/Main.tsx
+++ frontend/src/pages/Game/Main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Level1 from '../../components/Level1';
 import Level5 from '../../components/Level5';
 import Level6 from '../../components/Level6';
+
 
 const Main: React.FC = () => {
     return(
         <>
             <div>Hello</div>
+            <Level1 />
             <Level5 />
             <Level6 />
         </>
